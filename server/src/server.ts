cf27ec4c6aa4e7bc6733c67f4f2f15ee095f105e
Araceli Prieto Gallego <arpg@kth.se>
2024-11-08 16:47:30 +0100

    added cors error management

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import { productRoute } from './routes/productRoute';
 
 const server = express();
 
 server.use(express.json());
+server.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 server.use('/products', productRoute);
 
 server.listen(5050, () => {
