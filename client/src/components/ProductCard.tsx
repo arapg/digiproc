07400a2c0e82ae05a94173d53bb6ba97040c6be7
Araceli Prieto Gallego <arpg@kth.se>
2024-11-08 20:18:59 +0100

    added price of product and letterspacing

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Rating } from '@mui/material'
 import { Product } from '@/types/Product'
 import AddToCartButton from './AddToCartButton'
+import ProductPrice from './ProductPrice'
 
 interface ProductCardProps {
 	product: Product
@@ -10,20 +11,32 @@ interface ProductCardProps {
 
 function ProductCard({ product }: ProductCardProps) {
 	return (
-		<div className='bg-white rounded-md'>
-			<AddToCartButton color={product.color} />
-			<div>
+		<div className='bg-white rounded-md relative drop-shadow-xl'>
+			<div className='relative'>
 				<Image
 					src={product.imageURL}
 					alt={product.name}
 					width={200}
 					height={163}
 				/>
+				<div className='font-custom text-sm font-semibold tracking-tight bg-white px-[10px] py-[7px] rounded-full absolute bottom-1 right-1'>
+					<ProductPrice price={product.price} /> SEK
+				</div>
 			</div>
-			<div>
-				<p>{product.name}</p>
-				<p>Ratings</p>
-				<Rating value={product.rating} size='small' />
+			<div className='text-center p-2'>
+				<h3
+					className='text-sm font-custom font-semibold tracking-tight'
+					style={{ color: product.color }}
+				>
+					{product.name}
+				</h3>
+				<p className='text-xs font-normal tracking-tight'>Ratings</p>
+				<div>
+					<Rating value={product.rating} size='small' />
+				</div>
+				<div className=''>
+					<AddToCartButton color={product.color} />
+				</div>
 			</div>
 		</div>
 	)
