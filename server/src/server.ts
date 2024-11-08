bc4d435f01e0732ccf6a73100ceb835f71a9ab89
Araceli Prieto Gallego <arpg@kth.se>
2024-11-08 15:12:04 +0100

    removed old (non-ts) commented code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,3 @@
-// const express = require('express');
-// const { productRoute } = require('./routes/productRoute');
-
-// const server = express();
-
-// server.use(express.json());
-// server.use('/products', productRoute);
-
-// server.listen(5050);
-// console.log('Server is running at http://localhost:5050/');
-
 import express from 'express';
 import { productRoute } from './routes/productRoute';
 
