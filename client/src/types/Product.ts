962201f3fcd2addef11ec93c5ad4c8c6d6a86873
Araceli Prieto Gallego <arpg@kth.se>
2024-11-08 16:36:08 +0100

    added ID property to products

---
+++ client/src/types/Product.ts
@@ -0,0 +1,10 @@
+// client/types/Product.ts
+export interface Product {
+    id: number;
+    name: string;
+    rating: number;
+    price: number;
+    imageUrl: string;
+    color: string;
+  }
+  
