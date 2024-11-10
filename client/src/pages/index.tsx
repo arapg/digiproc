a62c5d28480ddfc2eb0ebb7647036b0935176c8a
Araceli Prieto Gallego <arpg@kth.se>
2024-11-10 12:55:44 +0000

    added cart component to index.tsx

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Cart from '@/components/Cart'
 import ProductCard from '@/components/ProductCard'
 import { useProducts } from '@/hooks/useProducts'
 
@@ -5,13 +6,16 @@ export default function Home() {
 	const { products, error } = useProducts()
 
 	return (
-		<div className='bg-slate-50 min-h-screen flex items-center justify-center'>
-			<div className='flex flex-wrap justify-center gap-16 max-w-screen-lg'>
-				{products &&
-					products.map((product) => (
-						<ProductCard key={product.id} product={product} />
-					))}
+		<div className='bg-slate-50 min-h-screen'>
+			<div className='flex items-center justify-center'>
+				<div className='flex flex-wrap justify-center gap-16 max-w-screen-lg'>
+					{products &&
+						products.map((product) => (
+							<ProductCard key={product.id} product={product} />
+						))}
+				</div>
 			</div>
+			<Cart />
 		</div>
 	)
 }
