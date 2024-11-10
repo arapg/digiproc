57087c2826ed48fc0cd1661bf3d5dfe56657e90d
Araceli Prieto Gallego <arpg@kth.se>
2024-11-10 12:42:47 +0000

    added onClick function to add to cart button

--- client/src/components/AddToCartButton.tsx
+++ client/src/components/AddToCartButton.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 interface AddToCartButtonProps {
 	color: string
+	onClick: () => void
 }
 
-function AddToCartButton({ color }: AddToCartButtonProps) {
+function AddToCartButton({ color, onClick }: AddToCartButtonProps) {
 	return (
 		<button
 			className={`font-custom font-bold text-xs text-white tracking-tight px-[51px] py-[11px] rounded-md cursor-pointer`}
 			style={{ background: color }}
+			onClick={onClick}
 		>
 			ADD TO CART
 		</button>
