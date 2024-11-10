54996c7212116ba2dac2863aa2421c7e85f58715
Araceli Prieto Gallego <arpg@kth.se>
2024-11-10 12:55:05 +0000

    added add to cart functionality in product card (and fixed spacing to match figma)

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -4,6 +4,7 @@ import { Rating } from '@mui/material'
 import { Product } from '@/types/Product'
 import AddToCartButton from './AddToCartButton'
 import ProductPrice from './ProductPrice'
+import { useCart } from '@/context/CartContext'
 
 interface ProductCardProps {
 	product: Product
@@ -11,6 +12,7 @@ interface ProductCardProps {
 
 function ProductCard({ product }: ProductCardProps) {
 	const [rating, setRating] = useState(product.rating)
+	const { addToCart } = useCart()
 
 	const handleRatingChange = (
 		event: React.SyntheticEvent,
@@ -21,8 +23,17 @@ function ProductCard({ product }: ProductCardProps) {
 		}
 	}
 
+	const handleAddToCart = () => {
+		addToCart({
+			id: product.id,
+			name: product.name,
+			price: product.price,
+			quantity: 1,
+		})
+	}
+
 	return (
-		<div className='bg-white rounded-md relative drop-shadow-xl w-[200px] transform transition-transform duration-200 hover:-translate-y-2'>
+		<div className='bg-white rounded-md relative drop-shadow-xl w-[200px] transform transition-transform duration-200 hover:scale-105'>
 			<div className='relative'>
 				<Image
 					src={product.imageURL}
@@ -30,7 +41,7 @@ function ProductCard({ product }: ProductCardProps) {
 					width={200}
 					height={163}
 				/>
-				<div className='font-custom text-sm font-semibold tracking-tight bg-white px-[10px] py-[7px] rounded-full absolute bottom-1 right-1'>
+				<div className='font-custom text-sm font-semibold tracking-tight bg-white px-[10px] py-[7px] rounded-full absolute bottom-1 right-1 text-[#1A364C]'>
 					<ProductPrice price={product.price} /> SEK
 				</div>
 			</div>
@@ -42,12 +53,14 @@ function ProductCard({ product }: ProductCardProps) {
 					{product.name}
 				</h3>
 
-				<div className='mb-4 mt-2'>
-					<p className='text-xs font-normal tracking-tight'>Ratings</p>
+				<div className='mb-4 mt-3'>
+					<p className='text-[10px] font-normal tracking-tight mb-[4px]'>
+						Ratings
+					</p>
 					<Rating value={rating} onChange={handleRatingChange} size='small' />
 				</div>
 				<div>
-					<AddToCartButton color={product.color} />
+					<AddToCartButton color={product.color} onClick={handleAddToCart} />
 				</div>
 			</div>
 		</div>
